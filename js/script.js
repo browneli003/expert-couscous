86012b78c95a6a0f88eba254014143309281ebde
Elia Brown <eliabrown@Elias-MacBook.local>
2021-07-10 01:15:29 -0600

    questions

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var questions = 
+[
+    {
+    question: "What tag is used to define a hyperlink, or link to another page?",
+    selections: ["strong", "blockquote", "em", "a"],
+    answer: "a"
+    },
+
+   {
+    question: "What tag is used to define a list item (in a bulleted list)?",
+    selections: ["ul", "li", "s", "u"],
+    answer: "li"
+    },
+
+   {
+    question: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+    selections: ["restriction", "scope", "output level", "range"],
+    answer: "scope"
+    },
+
+   {
+   question: "What are the CSS properties that are used to add space around sections of content?",
+   selections: ["break", "spacing", "padding", "cleaner"],
+   answer: "padding"
+    },
+
+    {
+    question: "What is the box called in CSS that wraps around every HTML element?",
+    selections: ["float", "boundary", "box model", "wrap"],
+    answer: "box model"
+    },
+
+];
